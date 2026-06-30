b9034125cb87d717ad404829266a41baf5f2f5a5
SatriaKusumadewa <satriadewa2002@gmail.com>
2026-06-30 21:37:01 +0700

    fix tailwind

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - Sistem Informasi SDM</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 <body class="min-h-screen bg-[#eef1f8]">
